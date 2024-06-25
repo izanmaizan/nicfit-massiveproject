8c697ddf7eedfc50a61224089df5f2ed3e6e8dcf
izanmaizan <izanmaizan@gmail.com>
2024-06-25 08:12:35 +0700

    update

--- src/sections/TemanSehat.jsx
+++ src/sections/TemanSehat.jsx
@@ -20,7 +20,7 @@ function TemanSehat() {
 
   const fetchUsername = async () => {
     try {
-      const response = await axios.get("http://localhost:3000/me", {
+      const response = await axios.get("https://nicfit-backend.vercel.app/me", {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("refresh_token")}`,
         },
